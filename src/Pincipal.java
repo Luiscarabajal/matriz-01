7b89609bc9605d21514d4ccf6139b2ee369195ed
luis <luispires.m@gmail.com>
2020-05-19 13:53:18 -0300

    Projeto criado

---
+++ src/Pincipal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class Pincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        Funcoes casa= new Funcoes();
+        
+        System.out.print("Voce tem cem"+casa.x);
+        
+    }
+    
+}
---
+++ test/calculadora/CalculadoraTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
